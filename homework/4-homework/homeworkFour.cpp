b1076400622657c3cdaac1ee21ea91ff190a68c7
cayden <cayden@egghead.io>
2019-09-13 17:58:12 -0500

    updated code

--- homework/4-homework/homeworkFour.cpp
+++ homework/4-homework/homeworkFour.cpp
@@ -5,47 +5,55 @@
 using namespace std;
 
 int main() {
-  double numberOne, numberTwo, numberThree;
-  double greatestValue, leastValue, middleValue;
+  double numberOne, numberTwo, numberThree;  // Input variables
 
   //Input portion of code that will take and store the values of all three numbers defined previously
   cout << "What is the first number?" << endl;
   cin >> numberOne;
+  cout << endl;
 
   cout << "What is the second number?" << endl;
   cin >> numberTwo;
+  cout << endl;
 
   cout << "What is the third number?" << endl;
   cin >> numberThree;
+  cout << endl;
 
   //Greatest value if statement testing
   if (numberOne > numberTwo && numberOne > numberThree) {
-    cout << endl;
     cout << "The greatest value you entered is: " << numberOne << endl;
+    cout << endl;
   }
   else if (numberTwo > numberOne && numberTwo > numberThree) {
-    cout << endl;
     cout << "The greatest value you entered is: " << numberTwo << endl;
+    cout << endl;
   }
   else if (numberThree > numberOne && numberThree > numberTwo) {
-    cout << endl;
     cout << "The greatest value you entered is: " << numberThree << endl;
+    cout << endl;
   }
-
-  cout << endl;
 
   //Least value if statement testing
   if (numberOne < numberTwo && numberOne < numberThree) {
-    cout << endl;
     cout << "The lowest value you entered is: " << numberOne << endl;
+    cout << endl;
   }
   else if (numberTwo < numberOne && numberTwo < numberThree) {
-    cout << endl;
     cout << "The lowest value you entered is: " << numberTwo << endl;
+    cout << endl;
   }
   else if (numberThree < numberOne && numberThree < numberTwo) {
-    cout << endl;
     cout << "The lowest value you entered is: " << numberThree << endl;
+    cout << endl;
+  }
+
+  // Middle value if statement testing
+  if ((numberOne > numberTwo && numberOne < numberThree) || (numberOne < numberTwo && numberOne > numberThree)) {
+    cout << "The middle value you entered is: " << numberOne << endl;
+  }
+  else if ((numberTwo > numberOne && numberTwo < numberThree) || (numberTwo < numberOne && numberTwo > numberThree)) {
+    
   }
 
   return 0;
